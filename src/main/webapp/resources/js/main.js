3203876944d845c1d790fc5ae9a5cf6b5f279863
Eugen Druta <eugen.druta@tup.com>
2019-02-13 17:06:45 +0100

    Reorg: cleanin

--- src/main/webapp/resources/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('#scramble').on('click', function (e) {
-    $('.scramble').encode();
-  });
-
-  $('#unscramble').on('click', function (e) {
-    $('.scramble').encode({action: 'unscramble'});
-  });
-});
